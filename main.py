25cd9383faec9c17ec61a57323696d4b83884a39
HarunaIzumi <haruharuna@outlook.com>
2025-05-22 19:33:12 -0300

    Added Lock/Unlock command

--- main.py
+++ main.py
@@ -19,20 +19,46 @@ bot = commands.Bot(command_prefix="?", intents=intents, log_handler=handler)
 async def on_ready():
     print(f"The bot is running as {bot.user}")
 
-@bot.command()
+@bot.command() #Adds two numbers together
 async def add(ctx, left: int, right: int):
-    """Adds two numbers together."""
     await ctx.send(left + right)
 
-@bot.command()
+@bot.command() #Information embed command
 async def info(ctx):
     embed = discord.Embed(
-        description = "This is the info page for **Nerissa's Little Jailbird** bot, created by <@156500926880940032>.\n\nThis is version: Alpha.",
+        description = "This is the info command for **Nerissa's Little Jailbird** bot, created by <@156500926880940032>.\n\nThis is version: Alpha.",
         color=discord.Color.blue()
     )
     embed.set_footer(icon_url=bot.user.avatar.url, text=f"{bot.user}")
 
     await ctx.send(embed=embed)
+
+@bot.command() #lock command
+async def lock(ctx):
+    channel = ctx.channel
+    overwrite = channel.overwrites_for(ctx.guild.default_role)
+
+    if overwrite.send_messages is False:
+        await ctx.send(f"<#{channel.id}> is already locked.")
+        return
+    
+    overwrite.send_messages = False
+    await channel.set_permissions(ctx.guild.default_role, overwrite=overwrite)
+    await ctx.send(f"<#{channel.id}> has been locked.")
+
+
+@bot.command() #unlock command
+async def unlock(ctx):
+    channel = ctx.channel
+    overwrite = channel.overwrites_for(ctx.guild.default_role)
+
+    if overwrite.send_messages is True or None:
+        await ctx.send(f"<#{channel.id}> is already unlocked.")
+        return
+
+    overwrite.send_messages = None
+    await channel.set_permissions(ctx.guild.default_role, overwrite=overwrite)
+    await ctx.send(f"<#{channel.id}> has been unlocked.")
 
 # Run bot with token
 bot.run(token)
