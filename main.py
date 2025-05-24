afb8da1018eef77e2ff8ee059fa10f86d25abc76
HarunaIzumi <haruharuna@outlook.com>
2025-05-24 20:28:24 -0300

    Updated ping, info and echo as slash commands Readded role trap

--- main.py
+++ main.py
@@ -1,16 +1,63 @@
 import discord
+from discord.ext import commands
+from discord import app_commands
 import os
 from dotenv import load_dotenv
 
+load_dotenv() # Loads .env file
+token = os.getenv("TOKEN") # Gets Token from .env file
+guild = discord.Object(os.getenv("GUILD_ID")) # Gets Guild ID for slash commands from .env file
+
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 
-client = discord.Client(intents=intents)
+client = commands.Bot(command_prefix='j?', intents=intents)
 
 @client.event
 async def on_ready():
     print(f"The bot is running as {client.user}")
+    try:
+        synced = await client.tree.sync(guild=guild)
+        print(f"Synced {len(synced)} commands to guild {guild.id}")
+    except Exception as e:
+        print (f"Error syncing commands: {e}")
 
-load_dotenv()
-token = os.getenv("TOKEN")
+@client.event # Onboarding role trap
+async def on_member_update(before, after):
+    roletrap = 1374928771584495689
+    roleupdate = set(after.roles) - set(before.roles)
+    for role in roleupdate:
+        if role.id == roletrap:
+            try:
+                await after.kick(reason="Onboarding role trap failed.")
+                print(f"Kicked {after.name} for failing the Onboarding role trap.")
+            except discord.Forbidden:
+                print(f"Error kicking {after.name}: Missing permissions.")
+            except Exception as e:
+                print(f"Error kicking {after.name}:{e}.")
+
+@client.tree.command(name="ping", description="Displays the bot latency", guild=guild)
+async def ping(interaction: discord.Interaction):
+    await interaction.response.send_message(f":ping_pong: **Pong!** Latency: {round(client.latency * 1000)}ms.")    
+
+@client.tree.command(name="info", description="Displays the bot's info", guild=guild)
+async def info(interaction: discord.Interaction):
+    embed = discord.Embed(
+        description = "This is the info command for **Nerissa's Little Jailbird** bot, created by <@156500926880940032>.\n\nThis is version: Alpha.",
+        color=discord.Color.blue()
+    )
+    embed.set_footer(icon_url=client.user.avatar.url, text=f"{client.user}")
+    await interaction.response.send_message(embed=embed)
+
+@client.tree.command(name="echo", description="Makes the bot send a message to a specific channel", guild=guild)
+async def echo(interaction: discord.Interaction, channel: discord.TextChannel, message:str):
+    try:
+        await channel.send(message)
+        await interaction.response.send_message(f"Sent!", ephemeral=True)
+    except discord.Forbidden:
+        await interaction.response.send_message(f"Couldn't send message: Missing permissions.", ephemeral=True)
+    except Exception as e:
+        await interaction.response.send_message(f"Couldn't send message: {e}.", ephemeral=True)
+
 client.run(token)
